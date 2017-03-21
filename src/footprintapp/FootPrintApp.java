3e3848eb7e80ad22eabd08b3c97678e3b2260926
Timothy <tabradford@liberty.edu>
2017-03-21 14:23:27 -0400

    Testing netbeans git...

--- src/footprintapp/FootPrintApp.java
+++ src/footprintapp/FootPrintApp.java
@@ -17,7 +17,7 @@ public class FootPrintApp {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        String s = "App just started!";
+        String s = "Trying to do version controll with netbeans...";
         System.out.print(s);
     }
 }
